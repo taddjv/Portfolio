0adc95a0f6a8e15e679672dfd742d53adaf7fa24
taddifer <taddifer@Tadds-MacBook-Pro.local>
2023-04-27 22:16:36 -0400

    email work

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,10 +1,40 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useGeneral } from "../../context";
 import { useSpring, animated } from "react-spring";
-import { inputFocusIn, inputFocusOut } from "../../helper";
+import emailjs from "@emailjs/browser";
+import { emailCheck } from "../../helper";
+
 import FTitle from "./FTitle";
 const ContactForm = () => {
   const { location, setLocation } = useGeneral();
+  const [errors, setErrors] = useState([]);
+  const form = useRef({
+    name: null,
+    subject: null,
+    email: null,
+    message: null,
+    errors: [],
+  });
+  const final = useRef({});
+
+  const send = (e) => {
+    e.preventDefault();
+
+    if (emailCheck(form.current).ok) {
+      emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        {
+          name: " dfghbdfgg",
+          message: "fdgdf",
+          email: "dfgdf@gmail.com",
+        },
+        process.env.REACT_APP_PUBLIC_KEY
+      );
+    } else {
+      setErrors(emailCheck(form.current).errors);
+    }
+  };
 
   const thing = useSpring({
     top: location === "up" ? "0" : "-100vh",
@@ -18,48 +48,111 @@ const ContactForm = () => {
 
   return (
     <animated.div style={thing} className="container1">
-      <div className="contact-form">
+      <form useRef={final} onSubmit={send} className="contact-form">
         <FTitle />
-        <div className="cf-data">
+        <div
+          className="cf-data"
+          onChange={(e) => {
+            setErrors([]);
+            form.current.name = e.target.value;
+          }}
+          onBlur={(e) => {
+            if (!e.target.value) {
+              e.target.className = "cf-input-i cf-input";
+              e.currentTarget.childNodes[0].style.bottom = "24px";
+            }
+          }}
+          onFocus={(e) => {
+            setErrors([]);
+            e.currentTarget.childNodes[0].style.bottom = "56px";
+            e.target.className = "cf-input-i cf-input-2";
+          }}
+        >
           <div className="cf-label">Name</div>
           <input
-            onFocus={() => inputFocusIn(1)}
-            onBlur={(e) => inputFocusOut(1, e)}
-            className="cf-input-i cf-input"
+            className={`cf-input-i ${
+              errors.includes("name") ? "cf-error-2" : "cf-input"
+            }`}
             type="text"
           ></input>
         </div>
-        <div className="cf-data">
+        <div
+          className="cf-data"
+          onChange={(e) => {
+            setErrors([]);
+            form.current.subject = e.target.value;
+          }}
+          onBlur={(e) => {
+            if (!e.target.value) {
+              e.target.className = "cf-input-i cf-input";
+              e.currentTarget.childNodes[0].style.bottom = "24px";
+            }
+          }}
+          onFocus={(e) => {
+            setErrors([]);
+            e.currentTarget.childNodes[0].style.bottom = "56px";
+            e.target.className = "cf-input-i cf-input-2";
+          }}
+        >
           <div className="cf-label">Subject</div>
-          <input
-            onFocus={() => inputFocusIn(2)}
-            onBlur={(e) => inputFocusOut(2, e)}
-            className="cf-input-i cf-input"
-            type="text"
-          ></input>
+          <input className="cf-input-i cf-input" type="text"></input>
         </div>
-        <div className="cf-data">
+        <div
+          className="cf-data"
+          onChange={(e) => {
+            setErrors([]);
+            form.current.email = e.target.value;
+          }}
+          onBlur={(e) => {
+            if (!e.target.value) {
+              e.target.className = "cf-input-i cf-input";
+              e.currentTarget.childNodes[0].style.bottom = "24px";
+            }
+          }}
+          onFocus={(e) => {
+            setErrors([]);
+            e.currentTarget.childNodes[0].style.bottom = "56px";
+            e.target.className = "cf-input-i cf-input-2";
+          }}
+        >
           <div className=" cf-label">Email</div>
           <input
-            onFocus={() => inputFocusIn(3)}
-            onBlur={(e) => inputFocusOut(3, e)}
-            className="cf-input-i cf-input"
+            className={`cf-input-i ${
+              errors.includes("email") ? "cf-error-2" : "cf-input"
+            }`}
             type="email"
           ></input>
         </div>
-        <div className="cf-data">
+        <div
+          className="cf-data"
+          onChange={(e) => {
+            setErrors([]);
+            form.current.message = e.target.value;
+          }}
+          onBlur={(e) => {
+            if (!e.target.value) {
+              e.target.className = "cf-input-m cf-input";
+              e.currentTarget.childNodes[0].style.bottom = "24px";
+            }
+          }}
+          onFocus={(e) => {
+            setErrors([]);
+            e.currentTarget.childNodes[0].style.bottom = "216px";
+            e.target.className = "cf-input-m cf-input-2";
+          }}
+        >
           <div className="cf-label">Message</div>
           <textarea
-            onFocus={() => inputFocusIn(4)}
-            onBlur={(e) => inputFocusOut(4, e)}
-            className="cf-input cf-input-m"
+            className={`cf-input-m ${
+              errors.includes("message") ? "cf-error-2" : "cf-input"
+            }`}
             type="text"
           ></textarea>
         </div>
-        <button className="cf-button" onClick={() => setLocation("center")}>
+        <button className="cf-button" type="submit">
           Send
         </button>
-      </div>
+      </form>
     </animated.div>
   );
 };
